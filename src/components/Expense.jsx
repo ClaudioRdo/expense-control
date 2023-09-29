0e81141a6a5692fb94aaa6bef32ed9c52f56b951
Robledo Claudio <claudio.n.robledo@gmail.com>
2023-09-29 13:40:41 -0300

    Adding swipe

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,12 +1,20 @@
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list';
+import "react-swipeable-list/dist/styles.css";
 import { formatDate } from "../helpers";
 
-import IconSaving from '../assets/img/icon-saving.svg';
-import IconFood from '../assets/img/icon-food.svg';
-import IconHealth from '../assets/img/icon-health.svg';
-import IconHouse from '../assets/img/icon-house.svg';
-import IconLeisure from '../assets/img/icon-leisure.svg';
-import IconSubscriptions from '../assets/img/icon-subscriptions.svg';
-import IconExpenses from '../assets/img/icon-expenses.svg';
+import IconSaving from '../assets/img/icon_saving.svg';
+import IconFood from '../assets/img/icon_food.svg';
+import IconHealth from '../assets/img/icon_health.svg';
+import IconHouse from '../assets/img/icon_house.svg';
+import IconLeisure from '../assets/img/icon_leisure.svg';
+import IconSubscriptions from '../assets/img/icon_subscriptions.svg';
+import IconExpenses from '../assets/img/icon_expenses.svg';
 
 const iconsDictionary = {
     saving: IconSaving,
@@ -18,20 +26,36 @@ const iconsDictionary = {
     subscriptions: IconSubscriptions,
 };
 
+const leadingActions = () => {
+
+};
+
+const trailingActions = () => {
+
+};
+
 const expense = ({ expense }) => {
     const { category, name, amount, id, date } = expense;
     return (
-        <div className="gasto sombra">
-            <div className="contenido-gasto">
-                <div className="descripcion-gasto">
-                    <img src={iconsDictionary[category]} alt="category" />
-                    <p className="categoria">{category}</p>
-                    <p className="nombre-gasto">{name}</p>
-                    <p className="fecha-gasto">Added: <span>{formatDate(date)}</span></p>
+        <SwipeableList>
+            <SwipeableListItem
+                leadingActions={leadingActions}
+                trailingActions={trailingActions}
+            >
+
+                <div className="gasto sombra">
+                    <div className="contenido-gasto">
+                        <div className="descripcion-gasto">
+                            <img src={iconsDictionary[category]} alt="category" />
+                            <p className="categoria">{category}</p>
+                            <p className="nombre-gasto">{name}</p>
+                            <p className="fecha-gasto">Added: <span>{formatDate(date)}</span></p>
+                        </div>
+                    </div>
+                    <p className="cantidad-gasto">${amount}</p>
                 </div>
-            </div>
-            <p className="cantidad-gasto">${amount}</p>
-        </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
